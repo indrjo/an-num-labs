8d65c4da03edde8d557b2c9f2051f097c13ff626
indrjo <dedej.indrjo@gmail.com>
2023-06-20 10:56:40 +0200

    piccola revisione

--- zeri/convergences.m
+++ zeri/convergences.m
@@ -1,17 +1,20 @@
 format long;
 
+% Dati del problema.
 f = @(x) (x-1)*log(x);
 df = @(x) log(x) + (x-1)/x;
 x0 = 1.5;
 tol = 1e-6;
 itmax = 100;
 
-fplot(f, [-2, 2], "LineWidth", 2); grid on;
+% Grafico della funzione.
+fplot(f, [-2, 2], "LineWidth", 2);
+grid on;
 print graph2.png
 
+% Collezione degli incrementi e relativi grafici.
 [errs1] = newton_errors(f, df, x0, 1, tol, itmax);
 [errs2] = newton_errors(f, df, x0, 2, tol, itmax);
-
 semilogy(errs1, "red", "LineWidth", 2);
 hold on;
 semilogy(errs2, "blue", "LineWidth", 2);
