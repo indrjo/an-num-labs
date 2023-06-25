e43fe0a246d6ee869749b403125a5fa60b7c9760
indrjo <dedej.indrjo@gmail.com>
2023-06-25 10:40:37 +0200

    ...

--- metodi-diretti/minquad_qr.m
+++ metodi-diretti/minquad_qr.m
@@ -1,7 +1,6 @@
 function [c] = minquad_qr(n, x, y)
   A = vandermonde(n, x');
-  % Dobbiamo risolvere il problema A'*Ac = A'y in c.
-  [Q R] = sgs(A);
+  [Q R] = qr(A);
   % Qui Q è una matrice m*m, mentre R è m*(n+1).
   % Ma R è trapeizoidale superiore, quindi
   Q = Q(:,1:n+1);
